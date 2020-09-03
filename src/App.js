655935749836d7e462babb7fd3fa4f30680a725e
João Moraes <joaovitordmoraes@gmail.com>
2020-09-03 14:42:04 -0300

    :sparkles: Finish ServiceFilter component with children

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import HeaderMain from './components/HeaderMain';
+import IconFilter from './components/IconFilter';
 import IconSearch from './components/IconSearch';
 import LogoMain from './components/LogoMain';
+import ServiceFilter from './components/ServiceFilter';
+import TitleMain from './components/TitleMain';
 
 function App() {
   return (
-    <HeaderMain>
-      <LogoMain />
-      <IconSearch />
-    </HeaderMain>
+    <>
+      <HeaderMain>
+        <LogoMain />
+        <IconSearch />
+      </HeaderMain>
+      <ServiceFilter>
+        <IconFilter />
+        <TitleMain>Guia de serviços</TitleMain>
+      </ServiceFilter>
+    </>
   );
 }
 
