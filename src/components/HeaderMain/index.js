a3d6e9e5e00fa4fccd8ff973a5344906a3561695
João Moraes <joaovitordmoraes@gmail.com>
2020-10-06 20:33:42 -0300

    :lipstick: Update HeaderMain styles

--- src/components/HeaderMain/index.js
+++ src/components/HeaderMain/index.js
@@ -3,14 +3,14 @@ import IconSearch from '../IconSearch';
 import LogoMain from '../LogoMain';
 
 const HeaderMain = styled.header`
-    background-color: #3c5268;
-    display: flex;
-    justify-content: space-between;
+  background-color: #3c5268;
+  display: flex;
+  justify-content: space-between;
+  padding: 16px 16px 26px 16px;
 
-    & > ${LogoMain}, & > ${IconSearch} {
-        width: 24px;
-    }
-    
+  & > ${LogoMain}, & > ${IconSearch} {
+    width: 24px;
+  }
 `;
 
 export default HeaderMain;
