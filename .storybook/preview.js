29b11297d3e6feef4c62492d3db5db583396860a
João Moraes <joaovitordmoraes@gmail.com>
2020-12-29 23:37:56 -0300

    ✨ Change storybook background

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,11 @@
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
+  backgrounds: {
+    default: 'Dark',
+    values: [{
+      name: 'Dark',
+      value: '#3c5268'
+    }]
+  }
 }
