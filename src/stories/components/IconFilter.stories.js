cd107355ffe7154fd8aa5d0b5ed5e802b9b12dfb
João Moraes <joaovitordmoraes@gmail.com>
2020-12-29 23:59:27 -0300

    ✨Add IconFilter component in storybook

---
+++ src/stories/components/IconFilter.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import IconFilter from '../../components/IconFilter';
+
+export default {
+    title: 'Components/IconFilter',
+    component: IconFilter
+};
+
+export const Default = () => <IconFilter />;
