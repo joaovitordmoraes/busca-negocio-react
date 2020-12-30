987187defb5c6ec75690570650083884c8b54f93
João Moraes <joaovitordmoraes@gmail.com>
2020-12-29 23:41:38 -0300

    ✨Add IconSearch component in storybook

---
+++ src/stories/components/IconSearch.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import IconSearch from '../../components/IconSearch';
+
+export default {
+    title: 'Components/IconSearch',
+    component: IconSearch
+};
+
+export const Default = () => <IconSearch />;
