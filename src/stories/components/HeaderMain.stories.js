7fe673971270e27037c1b06766dff3f1879726d0
João Moraes <joaovitordmoraes@gmail.com>
2020-12-29 23:51:48 -0300

    ✨ Add HeaderMain component in storybook

---
+++ src/stories/components/HeaderMain.stories.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import HeaderMain from '../../components/HeaderMain';
+import LogoMain from '../../components/LogoMain';
+import IconSearch from '../../components/IconSearch';
+
+export default {
+    title: 'Components/HeaderMain',
+    component: HeaderMain
+};
+
+export const Default = () => (
+  <HeaderMain>
+    <LogoMain />
+    <IconSearch />
+  </HeaderMain>
+);
