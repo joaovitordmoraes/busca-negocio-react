8121e3a23c788d1d11e8a9fdb06f1533940141c5
João Moraes <joaovitordmoraes@gmail.com>
2021-01-07 14:47:44 -0300

    ✨ Add ServiceFilter component in storybook

---
+++ src/stories/components/ServiceFilter.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ServiceFilter from '../../components/ServiceFilter';
+
+export default {
+    title: 'Components/ServiceFilter',
+    component: ServiceFilter
+}
+
+export const Default = () => <ServiceFilter />
