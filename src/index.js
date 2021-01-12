4e743ffa9c0e73a694976971718bf8038ca5408f
João Moraes <joaovitordmoraes@gmail.com>
2021-01-12 18:44:57 -0300

    :art: Create variable settings

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-import Reset from './styles/generic/reset';
-import Base from './styles/elements/base';
-import App from './App';
+import Reset from './styles/generic/reset'
+import Base from './styles/elements/base'
+import App from './App'
+import Settings from './styles/settings/settings'
 
 ReactDOM.render(
   <>
     <Reset />
+    <Settings />
     <Base />
     <App />
   </>,
   document.getElementById('root')
-);
+)
---
+++ src/styles/settings/settings.js
@@ -0,0 +1,11 @@
+import { createGlobalStyle } from 'styled-components'
+
+const Settings = createGlobalStyle`
+    :root {
+        --size-one: 8px;
+        --size-two: 10px;
+        --size-three: 16px;
+        --size-four: 26px;
+    }
+`
+export default Settings
